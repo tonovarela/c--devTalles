10d950a02be9b1c61a95bb18e5e15a318e851525
tonovarela <tonovarela@live.com>
2025-09-26 16:39:34 -0600

    Agregar método DataTypes y ajustar llamada en Main

---
+++ 01-Bases/DataTypes.cs
@@ -0,0 +1,26 @@
+partial class Program
+{
+    static void DataTypes()
+    {
+        Console.WriteLine("Data Types in C#");  
+        int integer = 42;
+        double decimaNumber = 3.1416;
+        bool isTrue = true;;
+        char character = 'A';
+        string text = "Hello, C#";
+
+
+
+        Console.WriteLine($"Integer: {integer}");
+        Console.WriteLine($"Decimal Number: {decimaNumber}");
+        Console.WriteLine($"Boolean: {isTrue}");
+        Console.WriteLine($"Character: {character}");
+        Console.WriteLine($"String: {text}");
+
+
+        
+
+
+        
+    }
+}
--- 01-Bases/Variables.cs
+++ 01-Bases/Variables.cs
@@ -1,9 +1,13 @@
 partial class Program {
     
-    static void Variables() {
+
+    static void Variables() {        
         int edad =10;
         string nombre ="Juan";        
         bool esEstudiante = true;
+        
         Console.WriteLine($"Nombre: {nombre}, Edad: {edad}, es Estudiante: {esEstudiante}");
     }
+
+    
 }
